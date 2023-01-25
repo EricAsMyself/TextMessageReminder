2cfb7db7955621f78f6bcafe2ff0d7b6a11bd528
eagleye123 <49323274+eagleye123@users.noreply.github.com>
2023-01-25 15:42:35 -0700

    Simplified design

--- Text.py
+++ Text.py
@@ -1,20 +1,8 @@
-
+from phone import Texting
 def main():
-    people = get_people()
-    send_message(people)
 
-
-def get_people():
-    return ['jacob', 'isaac']
-
-def send_message(people):
-    for person in people:
-        print("sending the message: " + person)
-        message = client.messages.create(
-            body="REMINDER" + message,
-            from_="+16802034912",
-            to=PHONE_NUMBER
-        )
+    phone = Texting()
+    phone.send_message("hello world")
 
 if __name__ == "__main__":
     main()
