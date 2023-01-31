9e668b524e5c3a1a7c9e90c3fa0255270cde4144
eagleye123 <49323274+eagleye123@users.noreply.github.com>
2023-01-31 16:25:12 -0700

    Added better databases, and better texting

--- SQLDB.py
+++ SQLDB.py
@@ -1,31 +1,69 @@
 import sqlite3
 
+class Database:
+    def __init__(self):
+        self.conn = sqlite3.connect("families.db")
+        self.cursor = self.conn.cursor()
+        
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS people (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                fname TEXT NOT NULL,
+                lname TEXT NOT NULL,
+                phone INTEGER NOT NULL
+            )
+        """)
+        
+    def add_person(self, fname, lname, phone_num):
+        self.cursor.execute(f"""
+            INSERT INTO people (fname, lname, phone)
+            VALUES ('{fname}','{lname}','{phone_num}')
+        """)
+        self.conn.commit()
+        
+    def remove_person(self, name):
+        self.cursor.execute("""
+            DELETE FROM people
+            WHERE name = ?
+        """, (name,))
+        self.conn.commit()
+        
+    def get_people(self):
+        self.cursor.execute("SELECT * FROM people")
+        return self.cursor.fetchall()
+        
+    def close(self):
+        self.conn.close()
 
 
 
 
+# class Database():
+#   def __init__(self):
+#     self.conn = sqlite3.connect('families.db')
+#     self.cursor = self.conn.cursor()
+#     self.cursor.execute('''
+#     CREATE TABLE IF NOT EXISTS `Person` (
+#       `Person_id` INT AUTO_INCREMENT PRIMARY KEY,
+#       `Fname` VARCHAR(45) NULL,
+#       `Lname` VARCHAR(45) NULL,
+#       `PhoneNumber` VARCHAR(45) NULL
+#     )
+#     ''')
 
+#   def add_person(self, first_name, last_name, phone_number):
+#     # Insert data
+#     # self.cursor.execute(f'''INSERT INTO Person (Fname, Lname, PhoneNumber)
+#     #  VALUES ('{first_name}', '{last_name}', '{phone_number}')''')
+#      self.cursor.execute(f'''INSERT INTO Person (Fname, Lname, PhoneNumber)
+#      VALUES ('Eric', 'Poole', '123')''')
+#     # self.conn.commit()
 
-class Database():
-  def __init__(self):
-    conn = sqlite3.connect('families.db')
-    self.cursor = conn.cursor()
-    self.cursor.execute('''
-    CREATE TABLE IF NOT EXISTS `Person` (
-      `Person_id` INT AUTO_INCREMENT PRIMARY KEY,
-      `Fname` VARCHAR(45) NULL,
-      `Lname` VARCHAR(45) NULL,
-      `PhoneNumber` VARCHAR(45) NULL
-    )
-    ''')
-  def add_person(self):
-    # Insert data
-    self.cursor.execute("INSERT INTO Person (Fname, Lname, PhoneNumber) VALUES ('Eric', 'Poole', '+15037268721')")
+#   def get_people(self):
+#     # Query data
+#     self.cursor.execute("SELECT * FROM Person")
+#     print(self.cursor.fetchall())
 
-  def get_people(self):
-    # Query data
-    return self.cursor.execute("SELECT * FROM Person")
-
-  def close(self):
-    self.conn.commit()
-    self.conn.close()
+#   def close(self):
+#     self.conn.commit()
+#     self.conn.close()
--- console.py
+++ console.py
@@ -1,14 +1,44 @@
 from phone import Texting
 from SQLDB import Database
 
+# TODO remove this!
+import os
+from dotenv import load_dotenv
+
+load_dotenv() # This loads in the environmental variables.
+
 
 def main():
-    message("")
-
-
-def message(message):
+    people_db = Database()
     phone = Texting()
-    phone.send_message("hello world")
+
+    people_db.add_person("Eric", "Poole", os.getenv('PHONE_NUMBER'))
+
+    result = people_db.get_people()
+
+    text_people(phone, result, "Welcome to the ward")
+
+
+
+def text_people(phone: Texting, people, message):
+    try:
+        people_texted = ""
+        for person in people:
+            phone.send_message(message, person[3])
+            people_texted += str(person[2])
+        phone.send_message(f"We sent a message out to: {people_texted}" ,os.getenv('PHONE_NUMBER'))
+    except Exception as e:
+        print(e)
+        message(phone, f"Error sending text messages {e}" ,os.getenv('PHONE_NUMBER'))
+
+
+
+def add_people(db :Database, fname, lname, phone):
+    db.add_person(fname,lname, phone) 
+    print("done getting people")
+
+def message(phone : Texting, message, phone_number):
+    phone.send_message(message, phone_number)
 
 if __name__ == "__main__":
     main()
