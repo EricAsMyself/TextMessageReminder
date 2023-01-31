2911bda67c037b4af9708a508f06d0fbd1e78af8
eagleye123 <49323274+eagleye123@users.noreply.github.com>
2023-01-31 16:28:13 -0700

    Removed try except

--- console.py
+++ console.py
@@ -21,15 +21,11 @@ def main():
 
 
 def text_people(phone: Texting, people, message):
-    try:
-        people_texted = ""
-        for person in people:
-            phone.send_message(message, person[3])
-            people_texted += str(person[2])
-        phone.send_message(f"We sent a message out to: {people_texted}" ,os.getenv('PHONE_NUMBER'))
-    except Exception as e:
-        print(e)
-        message(phone, f"Error sending text messages {e}" ,os.getenv('PHONE_NUMBER'))
+    people_texted = ""
+    for person in people:
+        phone.send_message(message, person[3])
+        people_texted += str(person[2])
+    phone.send_message(f"We sent a message out to: {people_texted}" ,os.getenv('PHONE_NUMBER'))
 
 
 
@@ -37,8 +33,6 @@ def add_people(db :Database, fname, lname, phone):
     db.add_person(fname,lname, phone) 
     print("done getting people")
 
-def message(phone : Texting, message, phone_number):
-    phone.send_message(message, phone_number)
 
 if __name__ == "__main__":
     main()
