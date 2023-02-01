26e120cc5fb2a4d0d4245f57a6961052eeb1b087
eagleye123 <49323274+eagleye123@users.noreply.github.com>
2023-02-01 08:09:38 -0700

    Made console better, and added better databse

--- SQLDB.py
+++ SQLDB.py
@@ -35,35 +35,3 @@ class Database:
     def close(self):
         self.conn.close()
 
-
-
-
-# class Database():
-#   def __init__(self):
-#     self.conn = sqlite3.connect('families.db')
-#     self.cursor = self.conn.cursor()
-#     self.cursor.execute('''
-#     CREATE TABLE IF NOT EXISTS `Person` (
-#       `Person_id` INT AUTO_INCREMENT PRIMARY KEY,
-#       `Fname` VARCHAR(45) NULL,
-#       `Lname` VARCHAR(45) NULL,
-#       `PhoneNumber` VARCHAR(45) NULL
-#     )
-#     ''')
-
-#   def add_person(self, first_name, last_name, phone_number):
-#     # Insert data
-#     # self.cursor.execute(f'''INSERT INTO Person (Fname, Lname, PhoneNumber)
-#     #  VALUES ('{first_name}', '{last_name}', '{phone_number}')''')
-#      self.cursor.execute(f'''INSERT INTO Person (Fname, Lname, PhoneNumber)
-#      VALUES ('Eric', 'Poole', '123')''')
-#     # self.conn.commit()
-
-#   def get_people(self):
-#     # Query data
-#     self.cursor.execute("SELECT * FROM Person")
-#     print(self.cursor.fetchall())
-
-#   def close(self):
-#     self.conn.commit()
-#     self.conn.close()
